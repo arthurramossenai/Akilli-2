219dd25ab3fa21e31cb44356d310664ca790bb03
arthurramossenai <arthur.ramos@ba.estudante.senai.br>
2026-04-14 14:51:53 -0300

    Delete Downloads directory

--- Downloads/Akilli-2-main/akilli_app/android/app/src/main/kotlin/com/example/akilli_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.akilli_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Downloads/Akilli-2-main/backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/api/login', authController.login);
-router.post('/cadastro', authController.cadastro);
-
-module.exports = router;
